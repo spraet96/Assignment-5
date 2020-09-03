dc7d4ad69a314d2197a1f566640fa17734fc0c78
spraet96 <j-olli@hotmail.com>
2020-09-03 09:39:58 +0000

    Initial commit

---
+++ max_int.py
@@ -0,0 +1,5 @@
+#Algorithm to find the maximum positive integer input by a user.
+#The User repeatedly inputs numbers until a negative value is entered.
+
+#Can use a while loop to keep asking for an int while the number is not negative.
+#When it's negative we stop and print the maximum  number.
